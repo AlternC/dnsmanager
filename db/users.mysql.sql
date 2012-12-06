72835d0831fe1cc7eb7c539a680e271c3539b9eb
Benjamin Sonntag <benjamin@sonntag.fr>
2012-12-06 21:06:24 +0100

    adding db dump

--- db/users.mysql.sql
+++ db/users.mysql.sql
@@ -1,10 +1,105 @@
-CREATE TABLE IF NOT EXISTS `users` (
+-- MySQL dump 10.13  Distrib 5.1.66, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: dnsmanager
+-- ------------------------------------------------------
+-- Server version	5.1.66-0ubuntu0.11.10.2-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `difflog`
+--
+
+DROP TABLE IF EXISTS `difflog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `difflog` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `server` int(10) unsigned NOT NULL,
+  `action` tinyint(4) NOT NULL,
+  `zone` varchar(255) NOT NULL,
+  `datec` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=410 DEFAULT CHARSET=latin1 COMMENT='Diff on AlternC servers';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `servers`
+--
+
+DROP TABLE IF EXISTS `servers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `servers` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user` int(10) unsigned NOT NULL,
+  `hostname` varchar(64) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `ip` varchar(128) NOT NULL,
+  `cacert` text NOT NULL,
+  `enabled` tinyint(4) NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  `lastcount` int(11) NOT NULL COMMENT 'Number of domain during last synchro. -1 for error',
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COMMENT='The AlternC servers of each user';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `uid` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `login` varchar(32) NOT NULL,
-  `pass` text NOT NULL COMMENT 'crypt',
+  `pass` varchar(80) NOT NULL COMMENT 'crypt',
   `email` varchar(255) NOT NULL,
   `enabled` tinyint(1) NOT NULL DEFAULT '0',
   `admin` tinyint(1) NOT NULL DEFAULT '0',
+  `validated` tinyint(3) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`uid`),
   UNIQUE KEY `login` (`login`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `zones`
+--
+
+DROP TABLE IF EXISTS `zones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `zones` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `zone` varchar(255) NOT NULL,
+  `server` int(10) unsigned NOT NULL,
+  `datec` datetime NOT NULL,
+  `enabled` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `zone` (`zone`)
+) ENGINE=MyISAM AUTO_INCREMENT=380 DEFAULT CHARSET=latin1 COMMENT='The list of zones to synchronize using AXFR from remote serv';
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-12-06 21:06:18
