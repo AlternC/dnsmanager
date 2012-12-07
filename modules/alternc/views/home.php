8443ba7cf814281c9d9b4536c337abbb78550948
Benjamin Sonntag <benjamin@sonntag.fr>
2012-12-07 10:35:51 +0100

    adding alternc module for production server of alternc.net

---
+++ modules/alternc/views/home.php
@@ -0,0 +1,16 @@
+
+<div style="float: right; margin: 30px;">
+<p><?=_("The domains you host in your server should have the following DNS servers:"); ?></p>
+<table>
+<tr><th>Name</th><th>IPv4</th><th>IPv6</th></tr>
+<tr><td style="padding-right: 15px; font-family: Courier-New, fixed;">ns1.alternc.net</td><td>91.194.60.82</td><td>2001:67c:288::82</td></tr>
+<tr><td style="padding-right: 15px; font-family: Courier-New, fixed;">ns2.alternc.net</td><td>96.126.102.48</td><td></td></tr>
+   <tr><td style="padding-right: 15px; font-family: Courier-New, fixed;">ns3.alternc.net</td><td>193.56.58.27</td><td>2001:67c:288::1000:27</td></tr>
+</table>
+</div>
+
+
+<p><?=_("<a href=\"https://alternc.org/\">AlternC</a> is a free software that allows you to control your hosting server easily. As such, it knows how to manage domain names."); ?><br />
+<?=_("However, many users don't have their own DNS servers, or not enough of them. AlternC team decided to provide anybody with free DNS service for their servers running AlternC.");  ?><br />
+<?=_("To use that service, create an account on alternc.net, and add your AlternC's server to your account. <a href=\"http://aide-alternc.org/\">Then read the instructions here</a> to know how to automatically synchronize your server's list of domain names with alternc.net"); ?></p>
+<hr />
