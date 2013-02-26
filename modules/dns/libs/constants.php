6355aa050826a171a5d49e31adc883bf1c9d06f4
Benjamin Sonntag <benjamin@sonntag.fr>
2013-02-26 10:23:16 +0100

    new missing constants

--- modules/dns/libs/constants.php
+++ modules/dns/libs/constants.php
@@ -8,6 +8,7 @@ define("DIFF_ACTION_ENABLED",4); // Zone enabled because conflict disappeared
 define("DIFF_ACTION_EMPTY",5); // domlist page returned empty file.
 define("DIFF_ACTION_STATS",6); // log entry is statistics
 define("DIFF_ACTION_DELETE_SERVER",7); // This server entry was deleted.
+define("DIFF_ACTION_MASTER",8); // Successfull reload of the master zone
 
 
 
